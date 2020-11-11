261dcc80486ebeda3289c30389b2960b0cfe5a25
Pratik Chauhan <pratik.chauhan@raweng.com>
2020-11-11 13:05:29 +0530

    Fourth and fifth commit

--- app/src/main/java/com/raweng/gittesting/MainActivity.kt
+++ app/src/main/java/com/raweng/gittesting/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val secondCommit = "second"
         val third = "second"
+        val fourth = "Fourth"
+        val fiftth= "Fifth"
     }
 }
